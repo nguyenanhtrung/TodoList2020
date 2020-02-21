b7ed97071e1d4675ec2b2c0e1ec27c22bd0d83a0
Digital <digital@Digitals-Mac-mini-3.local>
2020-02-21 17:41:14 +0700

    Add depedencies, make Main Screen UI

--- app/src/main/java/com/example/todolist2020/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/todolist2020/ui/home/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.todolist2020
+package com.example.todolist2020.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.todolist2020.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
